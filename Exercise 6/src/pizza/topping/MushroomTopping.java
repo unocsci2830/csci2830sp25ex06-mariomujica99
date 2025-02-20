e51a24142521c850eea5958068b2b689a2314d02
mariomujica99 <mariomujica99@gmail.com>
2025-02-20 16:34:37 -0600

    Exercise 6
    Packaging

---
+++ Exercise 6/src/pizza/topping/MushroomTopping.java
@@ -0,0 +1,7 @@
+package pizza.topping;
+
+public class MushroomTopping extends VeggieTopping {
+	public String toString() {
+		return "Mushroom Topping";
+	}
+}
---
+++ Exercise 6/src/pizza/topping/PepperTopping.java
@@ -0,0 +1,7 @@
+package pizza.topping;
+
+public class PepperTopping extends VeggieTopping {
+	public String toString() {
+		return "Pepper Topping";
+	}
+}
